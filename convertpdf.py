a0d8e50f4b793bfb4f4bc5658b83855bbd49f801
Pratch P <pratch@cs.cmu.edu>
2018-08-25 12:55:59 +0700

    python3 compatibility

--- convertpdf.py
+++ convertpdf.py
@@ -6,13 +6,13 @@ import img2pdf
 from os import listdir, walk
 from os.path import isfile, join
 
-import urllib2
-from BeautifulSoup import BeautifulSoup
+import urllib
+from bs4 import BeautifulSoup
 
 CURRENT = os.path.dirname(__file__)
 
 def download_images(url):
-    html = urllib2.urlopen(url).read()
+    html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html)
     title = 'pdf_images' #soup.title.string
     images = soup.findAll('img', {'class':'slide_image'})
@@ -30,7 +30,7 @@ def convert_pdf(url):
         f.extend(filenames)
         break
     f = ["%s/%s" % (url, x) for x in f]
-    print f
+    print(f)
 
     pdf_bytes = img2pdf.convert(f, dpi=300, x=None, y=None)
     doc = open('result.pdf', 'wb')
@@ -38,5 +38,5 @@ def convert_pdf(url):
     doc.close()
 
 if __name__ == "__main__":
-    url = raw_input('Slideshare URL : ')
+    url = input('Slideshare URL : ')
     download_images(url)
